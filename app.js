b2afd6939b1f4f595b7136adc9a44525251c000e
Parth <parthgupta.lit@gmail.com>
2026-07-02 01:07:11 +0530

    Just add Some styling using bootstrap

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
-const morgan = require("morgan");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -19,21 +19,18 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(morgan("tiny"));
-
-
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
-  console.log(campgrounds);
   res.render("campgrounds/index", { campgrounds });
 });
 app.get("/campgrounds/new", (req, res) => {
