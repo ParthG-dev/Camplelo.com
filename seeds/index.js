8056372a473b1d05f52f91248e0a00efcaddbe47
Parth <parthgupta.lit@gmail.com>
2026-07-03 14:18:54 +0530

    Did error handling ans smal tweaks

--- seeds/index.js
+++ seeds/index.js
@@ -25,13 +25,12 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-          location: `${cities[random1000].city}, ${cities[random1000].state}`,
-          title: `${sample(descriptors)} ${sample(places)}`,
-          image: `https://picsum.photos/400?random=${Math.random()}`,
-          description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
-          price,
-        });
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        })
         await camp.save();
     }
 }
