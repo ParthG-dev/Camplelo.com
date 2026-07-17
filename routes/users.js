efa30f1ba98fa0005211611641fb228a3ca32cd3
Parth <parthgupta.lit@gmail.com>
2026-07-17 12:59:21 +0530

    put some authorization

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
-const {storeReturnTo} = require('../middleware');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -29,17 +28,17 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-router.post('/login',storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'welcome back!');
-    const redirectUrl = res.locals.returnTo || '/campgrounds';
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
     res.redirect(redirectUrl);
 })
 
 router.get('/logout', (req, res) => {
-    req.logout(() => {
-        req.flash('success', "Goodbye!");
-        res.redirect('/campgrounds');
-    });
-});
+    req.logout();
+    req.flash('success', "Goodbye!");
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
