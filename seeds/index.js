8bd92658edcad55bf68a4037e8baf038f4f423ff
Parth <parthgupta.lit@gmail.com>
2026-07-26 01:13:09 +0530

    finally done

--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,10 @@
+if(process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
