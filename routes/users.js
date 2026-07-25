acfa022aeffd81d725ff149ad86d7a9b60a9e919
Parth <parthgupta.lit@gmail.com>
2026-07-25 23:59:52 +0530

    change file name middleware.js middlewares.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
-const { storeReturnTo } = require("../middleware");
+const { storeReturnTo } = require("../middlewares");
 const users = require('../controllers/users');
 
 
